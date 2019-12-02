45ce32f625c6b60d527c34caf11c66bccdf5f869
Peter Vel <petervel@gmail.com>
2019-12-02 23:27:09 +0100

    coffee to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,20 @@
-const clubs =
-  1:
-    name: "ADO Den Haag"
-  2:
-    name: "AFC Ajax"
-  3:
-    name: "AZ"
-  4:
-    name: "Emmen"
-  5:
-    name: "Feyenoord"
-  6:
-    name: "Fortuna Sittard"
-  7:
-    name: "FC Groningen"
-  8:
-    name: "sc Heerenveen"
-  9:
-    name: "Heracles Almelo"
-  10:
-    name: "PEC Zwolle"
-  11:
-    name: "PSV Eindhoven"
-  12:
-    name: "RKC Waalwijk"
-  13:
-    name: "Sparta Rotterdam"
-  14:
-    name: "FC Twente"
-  15:
-    name: "FC Utrecht"
-  16:
-    name: "Vitesse"
-  17:
-    name: "VVV Venlo"
-  18:
-    name: "Willem II"
+const clubs = {
+  1: { name: "ADO Den Haag" }
+  2: { name: "AFC Ajax" }
+  3: { name: "AZ" }
+  4: { name: "Emmen" }
+  5: { name: "Feyenoord" }
+  6: { name: "Fortuna Sittard" }
+  7: { name: "FC Groningen" }
+  8: { name: "sc Heerenveen" }
+  9: { name: "Heracles Almelo" }
+  10: { name: "PEC Zwolle" }
+  11: { name: "PSV Eindhoven" }
+  12: { name: "RKC Waalwijk" }
+  13: { name: "Sparta Rotterdam" }
+  14: { name: "FC Twente" }
+  15: { name: "FC Utrecht" }
+  16: { name: "Vitesse" }
+  17: { name: "VVV Venlo" }
+  18: { name: "Willem II" }
+}
