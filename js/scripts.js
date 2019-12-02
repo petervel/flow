dc3efce804011af2dd5a7c3535422e7ba418dbec
Peter Vel <petervel@gmail.com>
2019-12-02 23:34:31 +0100

    Hrmz

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
-const clubs = {
-  1: { name: "ADO Den Haag" },
-  2: { name: "AFC Ajax" },
-  3: { name: "AZ" },
-  4: { name: "Emmen" },
-  5: { name: "Feyenoord" },
-  6: { name: "Fortuna Sittard" },
-  7: { name: "FC Groningen" },
-  8: { name: "sc Heerenveen" },
-  9: { name: "Heracles Almelo" },
-  10: { name: "PEC Zwolle" },
-  11: { name: "PSV Eindhoven" },
-  12: { name: "RKC Waalwijk" },
-  13: { name: "Sparta Rotterdam" },
-  14: { name: "FC Twente" },
-  15: { name: "FC Utrecht" },
-  16: { name: "Vitesse" },
-  17: { name: "VVV Venlo" },
-  18: { name: "Willem II" }
-}
+const clubs = [
+  { name: "ADO Den Haag" },
+  { name: "AFC Ajax" },
+  { name: "AZ" },
+  { name: "Emmen" },
+  { name: "Feyenoord" },
+  { name: "Fortuna Sittard" },
+  { name: "FC Groningen" },
+  { name: "sc Heerenveen" },
+  { name: "Heracles Almelo" },
+  { name: "PEC Zwolle" },
+  { name: "PSV Eindhoven" },
+  { name: "RKC Waalwijk" },
+  { name: "Sparta Rotterdam" },
+  { name: "FC Twente" },
+  { name: "FC Utrecht" },
+  { name: "Vitesse" },
+  { name: "VVV Venlo" },
+  { name: "Willem II" }
+]
 
-document.body.onLoad = => {
-  for (club in clubs) {
+document.body.onload = function() {
+  clubs.forEach(function(club) {
     document.write(club.name);
-  }
+  });
 }
